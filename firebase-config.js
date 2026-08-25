0c7b7fc83c1c5a6cf3eec077c6870541a75cabb9
licitacioneslutec-cloud <licitaciones.lutec@gmail.com>
2026-08-25 18:18:13 -0500

    version7

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+/* Configuración del proyecto de Firebase.
+   Si algún dato quedó mal copiado, reemplaza este bloque completo por el que
+   aparece en: consola de Firebase › engranaje › Configuración del proyecto ›
+   Tus aplicaciones › lutectienda › Config. */
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDC94IN1AaswoGzk3JRALYIuBqud02lljg",
+  authDomain: "tiendalutec-24ba7.firebaseapp.com",
+  databaseURL: "https://tiendalutec-24ba7-default-rtdb.firebaseio.com",
+  projectId: "tiendalutec-24ba7",
+  storageBucket: "tiendalutec-24ba7.firebasestorage.app",
+  messagingSenderId: "645363542889",
+  appId: "1:645363542889:web:37e6b23e7c023ffe236bf8"
+};
